8690da700990189ee4e5267dcb4d971a1610efa8
aledwassell <aledwassell@googlemail.com>
2018-09-25 19:20:33 +0100

    It works!!!

--- beigestation/src/app/_components/footer/footer.component.ts
+++ beigestation/src/app/_components/footer/footer.component.ts
@@ -2,14 +2,26 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  template: `
+      <footer>
+          <div>
+              <h1>{{title}}</h1>
+          </div>
+          <div>
+              <a routerLink="/">
+                  <i class="fa fa-instagram"></i>
+              </a>
+          </div>
+      </footer>
+  `,
+  styleUrls: ['./footer.component.sass']
 })
 export class FooterComponent implements OnInit {
-
+  title: string = 'beigestation';
   constructor() { }
 
   ngOnInit() {
+    this.title = this.title.toUpperCase();
   }
 
 }
--- beigestation/src/app/_components/main/main.component.ts
+++ beigestation/src/app/_components/main/main.component.ts
@@ -2,8 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  template: `
+      <div class="main-container">
+          <div class="panel-one">Hot Nugs</div>
+          <div class="panel-two">Sexy Pasts</div>
+          <div class="panel-three">Crispy Chris</div>
+          <div class="panel-merch">Merch</div>
+      </div>
+  `,
+  styleUrls: ['./main.component.sass']
 })
 export class MainComponent implements OnInit {
 
